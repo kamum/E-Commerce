6985875dab912de09300ffa1ec2186e89e0ab0cd
kamum <jonathanmundi@gmail.com>
2021-09-27 16:14:49 +0200

    Commercejs products added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,31 +4,15 @@ import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
 import useStyles from './styles';
 
-const products = [
-    {
-        id: 1,
-        name: 'shoes',
-        description: 'Runing shoes',
-        price: '$5',
-        image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/be6f4f03-835b-4ed7-89ef-b90b933cc29a/scarpa-da-gara-su-strada-zoomx-vaporfly-next-2-m9HkHj.png'
-    },
-    {
-        id: 2,
-        name: 'Macbook',
-        description: 'Apple Mackbook',
-        price: '$10',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmuFvfdifyKrUHww7iYrtw7Yw8_Sr90fEvjA&usqp=CAU'
-    }
-];
 
-const Products = () => {
+const Products = ({ products }) => {
 
     const classes = useStyles();
 
     return (
         <main className={classes.content}>
             <div className={classes.toolbar} />
-            <Grid container justify="center" spacing={4}>
+            <Grid container justifyContent="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3} >
                         <Product product={product} />
